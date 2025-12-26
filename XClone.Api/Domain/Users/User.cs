9e4d549f794339f9b6478718861a3d986892e8f9
Lucas Silva <lucasilvabr@gmail.com>
2025-12-26 18:31:22 -0300

    Criação do AppDbContext e Criação da classe User

---
+++ XClone.Api/Domain/Users/User.cs
@@ -0,0 +1,39 @@
+﻿namespace XClone.Api.Domain.Users;
+
+public class User
+{
+    public Guid Id { get; private set; }
+
+    public string UserName { get; private set; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+
+    public string PasswordHash { get; private set; }
+
+    public DateTime CreationTime { get; private set; }
+
+    public bool IsDeleted { get; private set; }
+    public DateTime? DeletionTime { get; private set; }
+    public Guid? DeleterUserId { get; private set; }
+
+    private User() { } // EF
+
+    public User(string userName, string name, string email, string passwordHash)
+    {
+        Id = Guid.NewGuid();
+        UserName = userName;
+        Name = name;
+        Email = email;
+        PasswordHash = passwordHash;
+
+        CreationTime = DateTime.UtcNow;
+        IsDeleted = false;
+    }
+
+    public void Delete(Guid deleterUserId)
+    {
+        IsDeleted = true;
+        DeletionTime = DateTime.UtcNow;
+        DeleterUserId = deleterUserId;
+    }
+}
---
+++ XClone.Api/Infra/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace XClone.Api.Infra.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
